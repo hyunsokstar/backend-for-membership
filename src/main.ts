9eee9b95b026e98f9e1cb4373545033d4d2f7a4a
hyunsokstar <hyunsokstar@gmail.com>
2024-02-29 17:47:35 +0900

    update for login check

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthMiddleware } from './auth/middlewares/auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(9090);
 }
+
 bootstrap();
